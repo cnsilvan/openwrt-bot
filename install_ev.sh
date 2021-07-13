e61a5394b0f53009638d34803d06c627d0d0f7ca
cnsilvan <cnsilvan@gmail.com>
2021-07-13 17:14:40 +0800

    update

--- install_ev.sh
+++ install_ev.sh
@@ -5,6 +5,7 @@ sudo -E apt-get update
 sudo -E apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs gcc-multilib g++-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler antlr3 gperf swig
 sudo -E apt-get -y autoremove --purge
 sudo -E apt-get clean
+sudo timedatectl set-timezone "Asia/Shanghai"
 # sudo mkdir -p -m 777 /mnt/openwrt/bin /mnt/openwrt/build_dir/host /mnt/openwrt/build_dir/hostpkg /mnt/openwrt/dl /mnt/openwrt/feeds /mnt/openwrt/staging_dir
 # ln -s /mnt/openwrt/bin ./bin
 # mkdir -p ./build_dir
