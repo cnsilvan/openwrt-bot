f4dcfcb9b38c883c7992c3d5073e9a950417a49f
cnsilvan <cnsilvan@gmail.com>
2022-04-29 11:25:44 +0800

    update

--- common_load_lastest_version.sh
+++ common_load_lastest_version.sh
@@ -16,6 +16,7 @@ git clone https://github.com/cnsilvan/luci-app-unblockneteasemusic.git
 git clone https://github.com/tty228/luci-app-serverchan.git
 git clone https://github.com/fw876/helloworld.git
 git clone https://github.com/lisaac/luci-app-dockerman.git
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
 git clone https://github.com/vernesong/OpenClash.git
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 pushd OpenClash/luci-app-openclash/tools/po2lmo
